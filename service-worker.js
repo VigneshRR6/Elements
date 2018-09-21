f7de127ffe8e124cad3b89c3e5827c67707bf55b
Vignesh Ravindran <40809150+VigneshRR6@users.noreply.github.com>
2018-09-20 21:57:14 -0400

    Create service- worker.js
    Javascript for the actions of the service worker in order to handle the caching strategies that are uses in the progressive web application. A big thanks to Pete LePage in building this project.

---
+++ service-worker.js
@@ -0,0 +1,71 @@
+ar dataCacheName = 'weatherData-v1';
+var cacheName = 'weatherPWA-final-1';
+/* itmes that are being cached */
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/clear.png',
+  '/images/cloudy-scattered-showers.png',
+  '/images/cloudy.png',
+  '/images/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/images/partly-cloudy.png',
+  '/images/rain.png',
+  '/images/scattered-showers.png',
+  '/images/sleet.png',
+  '/images/snow.png',
+  '/images/thunderstorm.png',
+  '/images/wind.png'
+];
+/* updates the statues of  the service worker */
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  /* calls the service worker */
+  return self.clients.claim();
+});
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /* Cashe then network strategy begin implimented here*/
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /* Request for the app shell files are being made. Here "Cache, falling back to the network" offline strategy
+     * The app is asking for app shell files. In this scenario the app uses the 
+     * "is being used in order to provide offline functionality */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
